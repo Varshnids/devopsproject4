59f895185e452c899c2032382816cc5bfe496e44
Varshni D S <127740252+Varshnids@users.noreply.github.com>
2025-04-14 22:48:02 +0530

    Create deploy.sh

---
+++ bookstore/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+    echo hi123
+    chmod 777 build.sh
+    echo build
+    docker build -t test .
+    docker login -u varshni057 -p @Varshni@1407
+    docker tag test varshni057/devops4
+    docker push varshni057/devops4
